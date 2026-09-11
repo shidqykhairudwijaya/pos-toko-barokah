e270aede50f88097a3b615ea10b9cfd0d786a284
Shidqy <shidqykhairudwijayaa@gmail>
2026-09-11 09:46:16 +0700

    menambah route about

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,20 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
  
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth'])
     ->name('dashboard');
 
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::resource('categories', CategoryController::class);
+    Route::resource('products', ProductController::class);
+});
+
+Route::get('/about', function(){
+    return "Toko yang menyediakan banyak makanan";
 });
