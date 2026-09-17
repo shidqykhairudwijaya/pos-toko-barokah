159005101c1e8a14d84b7d0dd90f74d56d8595e1
shidqy <shidqykhairudwijayaa@gmail.com>
2026-09-17 11:18:31 +0700

    fix status

--- resources/views/components/badge.blade.php
+++ resources/views/components/badge.blade.php
@@ -2,8 +2,8 @@
 
 @php
     $colors = match ($status) {
-        'Aman' => 'bg=green-100 text-green-800',
-        'menipis' => 'bg-yellow-100 text-green-800',
+        'Aman' => 'bg-green-100 text-green-800',
+        'menipis' => 'bg-yellow-100 text-yellow-800',
         'Habis' => 'bg-red-100 text-red-800',
         default => 'bg-gray-100 text-gray-800'
     };
