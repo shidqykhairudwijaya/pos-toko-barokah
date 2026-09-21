e3b34bf48d8c9d3689c49167eddeaa998521975c
shidqy <shidqy@gmail.com>
2026-09-21 10:31:37 +0700

    praktikum 6

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,10 +1,31 @@
 <?php
-
+ 
 namespace App\Models;
-
+ 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
+ 
 class Product extends Model
 {
-    //
+    protected $fillable = ['category_id', 'code', 'name', 'unit', 'price', 'stock'];
+ 
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    protected function formattedPrice(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value, $attributes) =>
+                'Rp ' . number_format($attributes['price'], 0, ',', '.')
+        );
+    }
+ 
+    public function transactionDetails()
+    {
+        return $this->hasMany(TransactionDetail::class);
+    }
 }
+
