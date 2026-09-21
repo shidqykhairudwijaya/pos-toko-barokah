c73b668a919d0533fe775adf3b9c4993a1421654
shidqy <shidqy@gmail.com>
2026-09-21 09:38:14 +0700

    praktikum 6 Model Eloquent dan interaksi tabel

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function transaction() {
+        return $this->hasMany(Transaction::class);
+    }
 }
