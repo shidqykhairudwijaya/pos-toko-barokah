3f56b8289524927fc69902d9552e4004f1d63e89
shidqy <shidqy@gmail.com>
2026-09-22 15:31:37 +0700

    praktikum 6

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
 
 class CategorySeeder extends Seeder
 {
@@ -12,8 +13,13 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        
+        $categories = ['Sembako', 'Minuman', 'Makanan Ringan', 'Kebutuhan Rumah Tangga'];
+        foreach ($categories as $name) {
+            Category::create(['name' => $name]);
+        }
     }
+
+
 
 
 }
